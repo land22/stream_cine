c7d080ee6c83c562373e5ab6a8c497edaa624a01
land22 <landrywabo8@gmail.com>
2022-06-07 14:50:20 +0100

    Ajout de la gestion des rôles et les accès au diffenretes fionctionnalités

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -9,7 +9,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+ * @IsGranted("ROLE_SUPER_ADMIN")
+ */
 #[Route('/categorie')]
 class CategorieController extends AbstractController
 {
---
+++ src/Form/UserType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles', ChoiceType::class,
+            [   
+                'choices' => [
+                    'Utilisateur'=>'ROLE_USER',
+                    'Administrateur'=>'ROLE_SUPER_ADMIN'
+                ]
+            ])
+            ->add('nom')
+            ->add('prenom')
+        ;
+        $builder->get('roles')
+        ->addModelTransformer(new CallbackTransformer(
+            function ($rolesAsArray)
+            {
+                return implode(',', $rolesAsArray);
+            },
+            function ($rolesAsString)
+            {
+                return explode( ',', $rolesAsString);
+            }
+        ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
