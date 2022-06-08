c0e531577f5fe3f14859d262ceac4ce10a39d428
land22 <landrywabo8@gmail.com>
2022-06-09 00:57:54 +0100

    Création de la fonctionnalité d'ajout de commentaire sur un film par un utilisateur

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,6 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\VideoRepository;
 use App\Repository\CinemaRepository;
+use App\Entity\Video;
+use App\Entity\Commentaire;
+use App\Form\CommentaireType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\CommentaireRepository;
 
 class MainController extends AbstractController
 {
@@ -29,6 +34,26 @@ class MainController extends AbstractController
     {
         return $this->render('main/cinema.html.twig', [
             'cinemas' => $cinemaRepository->findAll()
+        ]);
+    }
+    #[Route('/main/video/details/{id}', name: 'app_main_video_details', methods: ['GET', 'POST'])]
+    public function videoDetail(Video $video, Request $request, CommentaireRepository $commentaireRepository): Response
+    {
+        $commentaire = new Commentaire();
+        $form = $this->createForm(CommentaireType::class, $commentaire);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $commentaire->setUser($this->getUser());
+            $commentaire->setVideo($video);
+            $commentaireRepository->add($commentaire, true);
+
+            return $this->redirectToRoute('app_main_video_details', ['id'=>$video->getId()], Response::HTTP_SEE_OTHER);
+            
+        }
+        return $this->render('main/video_detail.html.twig', [
+            'video' => $video,
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/ProjectionType.php
+++ src/Form/ProjectionType.php
@@ -6,15 +6,27 @@ use App\Entity\Projection;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Video;
+use App\Entity\Cinema;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class ProjectionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('heureProjection')
-            ->add('video')
-            ->add('cinema')
+            ->add('heureProjection', DateTimeType::class, [
+                'date_label' => 'Date de la projection',
+            ])
+            ->add('video', EntityType::class,[
+                'class'=>Video::class,
+                'choice_label'=>'titre'
+            ])
+            ->add('cinema', EntityType::class,[
+                'class'=>Cinema::class,
+                'choice_label'=>'nom'
+            ])
         ;
     }
 
